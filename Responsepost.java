36f6733288476fe54d4a95de7128b7a99a39af00
quangtrinh1312 <117561496+quangtrinh1312@users.noreply.github.com>
2022-11-06 13:57:05 +0700

    Add files via upload

---
+++ Responsepost.java
@@ -0,0 +1,44 @@
+package responsepost;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Responsepost
+ */
+@WebServlet("/Responsepost")
+public class Responsepost extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Responsepost() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String a= request.getParameter("a");
+		String b= request.getParameter("b");
+		String[] c= request.getParameterValues("acesstories");
+		response.setContentType("text/html;charset=UTF-8");
+		
+	}
+
+}
